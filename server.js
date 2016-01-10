465b9f411786f2a1819bec1172f69124b36304af
Sean Esteva <seangeleno@gmail.com>
2016-01-10 14:10:28 -0800

    separated concerns with seperate html/css folder and wrote a get request to mongodb imdb db and movies collection

--- server.js
+++ server.js
@@ -4,16 +4,33 @@ var express     = require('express')
 ,   MongoClient = require('mongodb').MongoClient
 ,   assert      = require('assert');
 
-app.engine('html', cons.swig);
+app.engine('html', cons.nunjucks);
 app.set('view engine'), 'html');
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views/html');
 app.use(express.bodyParser()); //registers middleware, allows POST requests
 app.use(app.router);
 
-// Handler for internal server errors
-function errorHandler(err, req, res, next) {
-  console.log(err.message);
-  console.log(err.stack);
-  res.status(500);
-  res.render('error_template)', { error: err});
-}
+
+MongoClient.connect('mongodb://localhost:27017/imdb', function( err, db ) {
+
+  assert.equal( null, err );
+  console.log("Successfully connected to MongoDB - oh yeah!");
+
+  app.get('/', function( req, res ){
+
+    db.collection('movies').find({}).toArray(function( err, docs){
+      res.render('movies', { 'movies': docs } );
+    });
+
+  });
+
+  app.use(function( req, res ){
+    res.sendStatus(404);
+  });
+
+  var server = app.listen(3000, function(){
+    var port = server.address().port;
+    console.log('Express serer listening on port %s. ' + port);
+  });
+
+});
